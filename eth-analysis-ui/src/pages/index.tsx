6778f5059c8d8bef5ba17de91a3bc17c42376686
Rurutia1027 <rurutia1027@gmail.com>
2025-01-24 17:07:49 +0800

    update typescript notes and add some source codes

--- eth-analysis-ui/src/pages/index.tsx
+++ eth-analysis-ui/src/pages/index.tsx
@@ -1,5 +1,9 @@
 // pages/index.tsx
+
 import React from 'react';
+import Script from 'next/script';
+import { SWRConfig } from 'swr'
+
 
 const Home: React.FC = () => {
   return <h1>Welcome to eth-analysis-ui</h1>;
