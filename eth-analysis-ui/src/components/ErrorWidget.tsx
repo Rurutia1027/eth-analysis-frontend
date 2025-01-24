7f7db2fa6e08c9f4e89f9b0841182fd32298cf43
Rurutia1027 <rurutia1027@gmail.com>
2025-01-25 00:11:08 +0800

    update common functions

---
+++ eth-analysis-ui/src/components/ErrorWidget.tsx
@@ -0,0 +1,27 @@
+import type { FC } from "react";
+import { divide } from "lodash";
+
+type Props = { title: string };
+
+const ErrorWidget: FC<Props> = ({ title }) => (
+  <div className="w-full rounded-lg border border-red-400 bg-slateus-700 p-8 font-roboto text-base font-light text-white">
+    <p className="mb-8 font-inter text-xs font-light uppercase tracking-widest text-slateus-200">
+      {title}
+    </p>
+    <p className="mb-4">An exception has occured</p>
+    <p className="mb-2">
+      We&apos;re sorry, this should never happen! If you&apos;ve seen this
+      before, or reloading doesn&apos;t fix it, you can help debug it by sending
+      a screenshot to
+      <a
+        className="ml-2 cursor-pointer text-slateus-200 underline active:brightness-90"
+        href="https://twitter.com/ultrasoundmoney/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        @ultrasoundmoney
+      </a>
+      .
+    </p>
+  </div>
+);
---
+++ eth-analysis-ui/src/components/Twemoji.tsx
@@ -0,0 +1,89 @@
+import React, {FC, ReactHTML, ReactNode, useEffect, Children, useState, useRef} from "react";
+import twemoji from "twemoji";
+
+const parseChild = (
+    child: HTMLElement,
+    imageClassName: HTMLImageElement["className"] | undefined,
+) => {
+    twemoji.parse(child, {
+        className: imageClassName,
+        ext: ".svg",
+        folder: "svg",
+        base: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/",
+    });
+};
+
+const Twemoji: FC<{
+    children: ReactNode;
+    className?: HTMLDivElement["className"];
+    imageClassName?: HTMLDivElement["className"];
+    wrapper?: boolean;
+    tag?: keyof ReactHTML;
+}> = ({
+          children,
+          className = "",
+          imageClassName,
+          wrapper = false,
+          tag = "div",
+      }) => {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const refList = useRef<HTMLElement[]>([]);
+    const [replaceDone, setReplaceDone] = useState(false);
+
+    useEffect(() => {
+        refList.current.map((childRef) => {
+            parseChild(childRef, imageClassName);
+        });
+        setReplaceDone(true);
+    }, [children, imageClassName, refList]);
+
+    useEffect(() => {
+        if (wrapperRef.current === null) {
+            return;
+        }
+
+        parseChild(wrapperRef.current, imageClassName);
+        setReplaceDone(true);
+    }, [children, wrapperRef, imageClassName]);
+
+    return children == null ? null : wrapper ? (
+        React.createElement(
+            tag,
+            {
+                ref: wrapperRef,
+                className: `${className}${replaceDone ? "" : " invisible"}`,
+            },
+            children,
+        )
+    ) : (
+        <>
+            {Children.map(children, (child, index) => {
+                if (child === null) {
+                    return null;
+                }
+                if (typeof child === "string") {
+                    return child;
+                }
+
+                if (typeof child === "boolean") {
+                    return child;
+                }
+
+                if (!React.isValidElement<HTMLElement>(child)) {
+                    return child;
+                }
+
+                return React.cloneElement(child as any, {
+                    ref: (ref: HTMLElement) => {
+                        refList.current[index] = ref;
+                    },
+                    className: `${child.props.className}${
+                        replaceDone ? "" : " invisible"
+                    }`,
+                });
+            })}
+        </>
+    );
+};
+
+export default Twemoji;
