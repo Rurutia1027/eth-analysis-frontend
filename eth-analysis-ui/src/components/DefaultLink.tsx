d9853b0502c508e9a4adefa827d2a26b2d158448
Rurutia1027 <rurutia1027@gmail.com>
2025-01-25 22:09:33 +0800

    add multiple components

---
+++ eth-analysis-ui/src/components/DefaultLink.tsx
@@ -0,0 +1,34 @@
+import type { FC, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+  href: string | undefined;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
+};
+
+const DefaultLink: FC<Props> = ({
+  children,
+  className = "",
+  href,
+  onMouseEnter,
+  onMouseLeave,
+}) => (
+  <a
+    className={`
+      ${href !== undefined ? "active:brightness-75" : ""}
+      ${href !== undefined ? "active:brightness-[85%]" : ""}
+      ${className}
+    `}
+    href={href}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+    rel="noreferrer"
+    target="_blank"
+  >
+    {children}
+  </a>
+);
+
+export default DefaultLink;
