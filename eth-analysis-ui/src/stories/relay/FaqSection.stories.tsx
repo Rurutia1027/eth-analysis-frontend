4ceb91e6c28a6b116c09f6695d1ef62169f1eeb9
Rurutia1027 <rurutia1027@gmail.com>
2025-01-31 18:11:27 +0800

    fix yarn build error

--- eth-analysis-ui/src/stories/relay/FaqSection.stories.tsx
+++ eth-analysis-ui/src/stories/relay/FaqSection.stories.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
 import FaqBlock from "../../relay/sections/FaqSection";
-import { NavigationContext } from "../../contexts/NavigationContext";
+import {
+  NavigationContext,
+  INavigationContext,
+} from "../../contexts/NavigationContext";
 import Accordion from "../../components/Accordion";
+
+/* eslint-disable @typescript-eslint/no-empty-function */
+const fullNavigationContext: INavigationContext = {
+  hidingNavigationPosition: 0,
+  changeHidingNavigationPosition: (value: number) => {
+    console.log(`Changing hidingNavigationPosition to ${value}`);
+  },
+  faqPosition: 0,
+  changeFaqPosition: (value: number) => {
+    console.log(`Changing faqPosition to ${value}`);
+  },
+};
 
 export default {
   title: "Components/FaqBlock",
   component: FaqBlock,
   decorators: [
     (Story) => (
-      <NavigationContext.Provider value={{ changeFaqPosition: () => {} }}>
+      <NavigationContext.Provider value={fullNavigationContext}>
         <Story />
       </NavigationContext.Provider>
     ),
@@ -50,7 +65,7 @@ export const MultipleFaqItems: StoryObj = {
     ];
 
     return (
-      <NavigationContext.Provider value={{ changeFaqPosition: () => {} }}>
+      <NavigationContext.Provider value={fullNavigationContext}>
         <section className="mt-64" id="faq">
           <div className="w-full px-4 md:px-8 lg:px-0">
             {mockCopy.map((item, index) => (
