63ac275b1a0f8db5c7017207b96077d92ad16123
Rurutia1027 <rurutia1027@gmail.com>
2025-02-09 22:29:18 +0800

    remove relay associated codes and focus on mainsite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Script from "next/script";
-import RelayIndexPage from "./relay"; // Ensure this points to the correct file
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome to eth-analysis-ui</h1>
-      <RelayIndexPage />
     </div>
   );
 };
