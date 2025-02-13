c48e15d518e04e109d09c347220eee1c3555e914
Rurutia1027 <rurutia1027@gmail.com>
2025-02-13 16:11:36 +0800

    add test cases for axios data fetchers

--- src/__tests__/axios-data-fetcher.test.ts
+++ src/__tests__/axios-data-fetcher.test.ts
@@ -1,12 +1,32 @@
 import axios from 'axios'; 
+import { fetchApiJson, ApiResult } from '../utils/axios-fetchers';
+import { BaseFeeOverTime } from '../api/base-fee-over-time';
+import { assert } from 'console';
 
 describe('fetchBaseFeeOverTime', () => { 
- it('logs data when the API call is successful', async () => { 
+ it('logs data when the API call is successful', async () => {
   try {
    const response = await axios.get('https://ultrasound.money/api/v2/fees/base-fee-over-time');
    console.log('Data: ', response.data);
-  } catch (error) { 
-   console.log('Error fetching the data: ', error); 
+  } catch (error) {
+   console.log('Error fetching the data: ', error);
   }
- })
+ }); 
+  it('fetch data via axios implemented axios-fetchers::fetchApiJson', async () => {
+   try {
+    const apiDomain = "https://ultrasound.money"; 
+    const url = "/api/v2/fees/base-fee-over-time"; 
+    const data_records: ApiResult<BaseFeeOverTime> = await fetchApiJson(apiDomain, url); 
+   
+    if ('data' in data_records) {
+     const { d1, d30, d7, h1, m5, since_burn, since_merge } = data_records.data;
+     assert(d1.length > 0); 
+     assert(d30.length > 0); 
+    } else { 
+     console.error("API Error: ", data_records.error); 
+    }
+  } catch (error) {
+   console.log('Error fetching the data: ', error);
+  }
+ }); 
 })
