184f5c0caa42602075d4e47bbcf0ec83d65ded3c
Rurutia1027 <rurutia1027@gmail.com>
2025-02-13 22:39:18 +0800

    comment JSBI for temporate

--- src/api/supply-parts.ts
+++ src/api/supply-parts.ts
@@ -1,98 +1,98 @@
-import * as DateFns from "date-fns";
-import JSBI from "jsbi";
-import { useEffect, useRef, useState } from "react";
-import useSWR from "swr";
-import type { Slot } from "../constants/beacon-units";
-import * as Duration from "../constants/duration";
-import type { EthNumber, GweiString, WeiJSBI } from "../constants/eth-units";
-import { WEI_PER_ETH } from "../constants/eth-units";
-import { WEI_PER_GWEI_JSBI } from "../constants/eth-units";
-import type { ApiResult } from "../utils/axios-fetchers";
-import { fetchApiJson, fetchJsonSwr } from "./fetchers";
+// import * as DateFns from "date-fns";
+// import JSBI from "jsbi";
+// import { useEffect, useRef, useState } from "react";
+// import useSWR from "swr";
+// import type { Slot } from "../constants/beacon-units";
+// import * as Duration from "../constants/duration";
+// import type { EthNumber, GweiString, WeiJSBI } from "../constants/eth-units";
+// import { WEI_PER_ETH } from "../constants/eth-units";
+// import { WEI_PER_GWEI_JSBI } from "../constants/eth-units";
+// import type { ApiResult } from "../utils/axios-fetchers";
+// import { fetchApiJson, fetchJsonSwr } from "./fetchers";
 
-export type SupplyPartsF = {
-  beaconBalancesSum: GweiString;
-  beaconDepositsSum: GweiString;
-  executionBalancesSum: GweiString;
-  slot: Slot;
-};
+// export type SupplyPartsF = {
+//   beaconBalancesSum: GweiString;
+//   beaconDepositsSum: GweiString;
+//   executionBalancesSum: GweiString;
+//   slot: Slot;
+// };
 
-export type SupplyParts = {
-  beaconBalancesSum: WeiJSBI;
-  beaconDepositsSum: WeiJSBI;
-  executionBalancesSum: WeiJSBI;
-  slot: number;
-};
+// export type SupplyParts = {
+//   beaconBalancesSum: WeiJSBI;
+//   beaconDepositsSum: WeiJSBI;
+//   executionBalancesSum: WeiJSBI;
+//   slot: number;
+// };
 
-export const decodeEthSupply = (supplyParts: SupplyPartsF): SupplyParts => ({
-  beaconBalancesSum: JSBI.multiply(
-    JSBI.BigInt(supplyParts.beaconBalancesSum),
-    WEI_PER_GWEI_JSBI,
-  ),
-  beaconDepositsSum: JSBI.multiply(
-    JSBI.BigInt(supplyParts.beaconDepositsSum),
-    WEI_PER_GWEI_JSBI,
-  ),
-  executionBalancesSum: JSBI.BigInt(supplyParts.executionBalancesSum),
-  slot: supplyParts.slot,
-});
+// export const decodeEthSupply = (supplyParts: SupplyPartsF): SupplyParts => ({
+//   beaconBalancesSum: JSBI.multiply(
+//     JSBI.BigInt(supplyParts.beaconBalancesSum),
+//     WEI_PER_GWEI_JSBI,
+//   ),
+//   beaconDepositsSum: JSBI.multiply(
+//     JSBI.BigInt(supplyParts.beaconDepositsSum),
+//     WEI_PER_GWEI_JSBI,
+//   ),
+//   executionBalancesSum: JSBI.BigInt(supplyParts.executionBalancesSum),
+//   slot: supplyParts.slot,
+// });
 
-export const ethSupplyFromParts = (supplyParts: SupplyParts): WeiJSBI =>
-  JSBI.subtract(
-    JSBI.add(supplyParts.executionBalancesSum, supplyParts.beaconBalancesSum),
-    supplyParts.beaconDepositsSum,
-  );
+// export const ethSupplyFromParts = (supplyParts: SupplyParts): WeiJSBI =>
+//   JSBI.subtract(
+//     JSBI.add(supplyParts.executionBalancesSum, supplyParts.beaconBalancesSum),
+//     supplyParts.beaconDepositsSum,
+//   );
 
-export const impreciseEthSupplyFromParts = (
-  ethSupplyParts: SupplyParts,
-): EthNumber => {
-  const ethSupplySum = ethSupplyFromParts(ethSupplyParts);
-  return JSBI.toNumber(ethSupplySum) / WEI_PER_ETH;
-};
+// export const impreciseEthSupplyFromParts = (
+//   ethSupplyParts: SupplyParts,
+// ): EthNumber => {
+//   const ethSupplySum = ethSupplyFromParts(ethSupplyParts);
+//   return JSBI.toNumber(ethSupplySum) / WEI_PER_ETH;
+// };
 
-export const fetchSupplyParts = (): Promise<ApiResult<SupplyPartsF>> =>
-  fetchApiJson("/api/v2/fees/supply-parts");
+// export const fetchSupplyParts = (): Promise<ApiResult<SupplyPartsF>> =>
+//   fetchApiJson("/api/v2/fees/supply-parts");
 
-export const useSupplyParts = (): SupplyParts => {
-  const { data } = useSWR<SupplyPartsF>(
-    "/api/v2/fees/supply-parts",
-    fetchJsonSwr,
-    {
-      refreshInterval: Duration.millisFromSeconds(4),
-    },
-  );
+// export const useSupplyParts = (): SupplyParts => {
+//   const { data } = useSWR<SupplyPartsF>(
+//     "/api/v2/fees/supply-parts",
+//     fetchJsonSwr,
+//     {
+//       refreshInterval: Duration.millisFromSeconds(4),
+//     },
+//   );
 
-  // We use an SWRConfig with fallback data for this hook.
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  return decodeEthSupply(data!);
-};
+//   // We use an SWRConfig with fallback data for this hook.
+//   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+//   return decodeEthSupply(data!);
+// };
 
-export const useImpreciseEthSupply = (): EthNumber | undefined => {
-  const ethSupply = useSupplyParts();
-  const lastRefresh = useRef<Date>();
-  const [lastEthSupply, setLastEthSupply] = useState<number>();
+// export const useImpreciseEthSupply = (): EthNumber | undefined => {
+//   const ethSupply = useSupplyParts();
+//   const lastRefresh = useRef<Date>();
+//   const [lastEthSupply, setLastEthSupply] = useState<number>();
 
-  useEffect(() => {
-    if (ethSupply === undefined) {
-      return undefined;
-    }
+//   useEffect(() => {
+//     if (ethSupply === undefined) {
+//       return undefined;
+//     }
 
-    if (
-      lastRefresh.current === undefined ||
-      DateFns.differenceInSeconds(new Date(), lastRefresh.current) > 60
-    ) {
-      lastRefresh.current = new Date();
-      setLastEthSupply(getEthSupplyImprecise(ethSupply));
-    }
-  }, [ethSupply]);
+//     if (
+//       lastRefresh.current === undefined ||
+//       DateFns.differenceInSeconds(new Date(), lastRefresh.current) > 60
+//     ) {
+//       lastRefresh.current = new Date();
+//       setLastEthSupply(getEthSupplyImprecise(ethSupply));
+//     }
+//   }, [ethSupply]);
 
-  return lastEthSupply;
-};
+//   return lastEthSupply;
+// };
 
-export const getEthSupplyImprecise = (ethSupply: SupplyParts): EthNumber =>
-  JSBI.toNumber(
-    JSBI.subtract(
-      JSBI.add(ethSupply.executionBalancesSum, ethSupply.beaconBalancesSum),
-      ethSupply.beaconDepositsSum,
-    ),
-  ) / 1e18;
+// export const getEthSupplyImprecise = (ethSupply: SupplyParts): EthNumber =>
+//   JSBI.toNumber(
+//     JSBI.subtract(
+//       JSBI.add(ethSupply.executionBalancesSum, ethSupply.beaconBalancesSum),
+//       ethSupply.beaconDepositsSum,
+//     ),
+//   ) / 1e18;
--- src/constants/eth-units.ts
+++ src/constants/eth-units.ts
@@ -1,27 +1,35 @@
 import JSBI from "jsbi";
 
-export const WEI_PER_GWEI_JSBI = JSBI.exponentiate(
-  JSBI.BigInt("10"),
-  JSBI.BigInt("9"),
-);
+// export const WEI_PER_GWEI_JSBI = JSBI.exponentiate(
+//   JSBI.BigInt(10),
+//   JSBI.BigInt(9),
+// );
 
 export const WEI_PER_ETH = 1e18;
 export const WEI_PER_GWEI = 1e9;
+
 export const GWEI_PER_ETH = 1e9;
+
 export type Gwei = number;
 export type Eth = number;
 
+// Some values are too big to safely fit in Number.MAX_SAFE_INTEGER (2^53) but we do want to display accurately, we use strings and JSBI for those.
 export type WeiString = string;
 export type WeiJSBI = JSBI;
 
+// Some values are too big to safely fit in Number.MAX_SAFE_INTEGER (2^53) but we do want to display accurately, we use strings and JSBI for those.
 export type GweiString = string;
 export type GweiJSBI = JSBI;
 
+// When precision is no issue or the precise number will fit in 2^53 for the foreseeable future, we use number.
 export type WeiNumber = number;
+
 export type GweiNumber = number;
+
 export type EthNumber = number;
 
-export type EthUseAmount = {
+// When a number is available in both ETH and USD.
+export type EthUsdAmount = {
   eth: EthNumber;
   usd: number;
 };
